e66deed6022b0e9d72e5fdc60c12fe668d262c61
CarlosGomezFernandez <carlos.gomez33@alu.uclm.es>
2021-11-17 12:08:24 +0100

    Primera prueba, soy Carlos Gómez

--- PruebaMaven/src/main/java/B/PruebaMaven/App.java
+++ PruebaMaven/src/main/java/B/PruebaMaven/App.java
@@ -3,5 +3,6 @@ package B.PruebaMaven;
 public class App {
   public static void main(String[] args) {
     System.out.println("Hello World!");
+    System.out.println("Primera prueba, soy Carlos Gómez");
   }
 }
